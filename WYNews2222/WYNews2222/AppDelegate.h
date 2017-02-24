b02f5282bb9ee3e8ea2db70579b49f6bff3e03e8
周明皓 <847710682@qq.com>
2017-02-24 00:21:33 -0800

    初始化项目

---
+++ WYNews2222/WYNews2222/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WYNews2222
+//
+//  Created by mac on 17/2/24.
+//  Copyright © 2017年 macd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
