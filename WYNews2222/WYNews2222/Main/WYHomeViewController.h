05879136dd905b713e49cab07d836131cb4a40be
周明皓 <847710682@qq.com>
2017-02-24 01:20:04 -0800

    修改程序目录和程序入口

--- WYNews2222/WYNews2222/Main/WYHomeViewController.h
+++ WYNews2222/WYNews2222/Main/WYHomeViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  WYHomeViewController.h
 //  WYNews2222
 //
 //  Created by mac on 17/2/24.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface WYHomeViewController : UIViewController
 
 @end
-
